3b1b144474af8190a772f3e58731b8840f6b45bd
Svitlana Dmytrenko <zeleneyabluko@gmail.com>
2024-03-07 12:50:01 +0200

    Revert "removed things I don't need"
    This reverts commit c1f4f338181a6e601f9b03545119f83d9d6b7087.

---
+++ src/uploadFile.js
@@ -0,0 +1,23 @@
+/*
+Need to fix the below function:
+
+function uploadFile() {
+
+
+    if (file) {
+        // Use FileReader to read the file content
+        
+
+            // Save the content to local storage (this is a simplified example)
+            localStorage.setItem('musicXmlContent', musicXmlContent);
+
+            alert('File uploaded and saved to local storage.');
+        };
+
+        reader.readAsText(file);
+    } else {
+        alert('Please select a valid *.musicxml file.');
+    }
+}
+
+*/
