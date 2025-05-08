52e54e5371cff78556d88fd10691c843c0a8fc8a
= <=>
2025-05-08 09:04:48 +0300

    chart sample done

--- src/soundFrequencyChart.js
+++ src/soundFrequencyChart.js
@@ -3,9 +3,11 @@ import Chart from "chart.js/auto";
 (async function () {
   const data = [
     { x: 0, y: 220 },
-    { x: 3, y: 250 },  // Step lasted 3 seconds
-    { x: 4, y: 230 },  // Step lasted 1 second
+    { x: 3, y: 250 }, 
+    { x: 4, y: 250 }, // Step lasted 3 seconds
+    { x: 4, y: null },  // Step lasted 1 second
     { x: 6, y: 240 },  // Step lasted 2 seconds
+    { x: 7, y: 220 },
   ];
 
   new Chart(document.getElementById("soundFrequencyChart"), {
