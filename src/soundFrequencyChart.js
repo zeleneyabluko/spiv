23160b979f5a281a83c50a969988cc47a686fb80
= <=>
2025-05-12 08:53:59 +0300

    removed unused code

--- src/soundFrequencyChart.js
+++ src/soundFrequencyChart.js
@@ -3,10 +3,11 @@ import Chart from "chart.js/auto";
 (async function (data) {
   console.log('data: ', data);
   console.log('started rendering chart|!');
-
-  console.log('canvas is found: ', document.getElementById("soundFrequencyChart"))
+  
   const canvas = document.getElementById("soundFrequencyChart");
-  canvas.style.display = 'block'; // Ensure it's visible
+  if(canvas){
+    console.log('Canvas found!');
+  }
 
   const ctx = canvas.getContext('2d');
   console.log('context: ', ctx);
