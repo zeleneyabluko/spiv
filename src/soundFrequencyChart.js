6ceec65c51f5447b552fe526e94c246fa0b047fb
= <=>
2025-05-19 06:44:24 +0300

    added license info

--- src/soundFrequencyChart.js
+++ src/soundFrequencyChart.js
@@ -1,4 +1,10 @@
 import { lightningChart } from "@lightningchart/lcjs";
 
-const lc = lightningChart({ license: 'my-license-key' })
+const lc = lightningChart({
+    license: "0002-n0i9AP8MN/ezP+gV3RZRzNiQvQvBKwBJvTnrFTHppybuCwWuickxBJV+q3qyoeEBGSE4hS0aeo3pySDywrb/iIsl-MEUCIAiJOU3BrUq71LqSlRAIFAI0dKK05qBRIJYHFmBoOoIHAiEA4Y55O1QpeuEkiuVktPGLauOHc1TzxNu85/vz/eNscz8=",
+    licenseInformation: {
+        appTitle: "LightningChart JS Trial",
+        company: "LightningChart Ltd."
+    },
+})
 const chart = lc.ChartXY();
