d612a6150a9a36f80f8d2ddff26519c4944b599f
= <=>
2025-05-19 07:08:36 +0300

    chart displays upon page loading

--- src/soundFrequencyChart.js
+++ src/soundFrequencyChart.js
@@ -1,28 +1,28 @@
 import { lightningChart } from "@lightningchart/lcjs";
 
-console.log('started rendering the chart!');
-const lc = lightningChart({
-    license: "0002-n0i9AP8MN/ezP+gV3RZRzNiQvQvBKwBJvTnrFTHppybuCwWuickxBJV+q3qyoeEBGSE4hS0aeo3pySDywrb/iIsl-MEUCIAiJOU3BrUq71LqSlRAIFAI0dKK05qBRIJYHFmBoOoIHAiEA4Y55O1QpeuEkiuVktPGLauOHc1TzxNu85/vz/eNscz8=",
-    licenseInformation: {
-        appTitle: "LightningChart JS Trial",
-        company: "LightningChart Ltd."
-    },
-})
-const chart = lc
-  .ChartXY({
-    // Set the chart into a div with id, 'target'.
-    // Chart's size will automatically adjust to div's size.
-    container: "soundFrequencyChart",
-  })
-  .setTitle("My first chart"); // Set chart title
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('started rendering the chart!');
+    const lc = lightningChart({
+        license: "0002-n0i9AP8MN/ezP+gV3RZRzNiQvQvBKwBJvTnrFTHppybuCwWuickxBJV+q3qyoeEBGSE4hS0aeo3pySDywrb/iIsl-MEUCIAiJOU3BrUq71LqSlRAIFAI0dKK05qBRIJYHFmBoOoIHAiEA4Y55O1QpeuEkiuVktPGLauOHc1TzxNu85/vz/eNscz8=",
+        licenseInformation: {
+            appTitle: "LightningChart JS Trial",
+            company: "LightningChart Ltd."
+        },
+    })
+    const chart = lc
+        .ChartXY({
+            container: "soundFrequencyChart",
+        })
+        .setTitle("My first chart");
 
-const data = [
-  { x: 0, y: 1.52 },
-  { x: 1, y: 1.56 },
-  { x: 2, y: 1.42 },
-  { x: 3, y: 1.85 },
-  { x: 4, y: 1.62 },
-];
+    const data = [
+        { x: 0, y: 1.52 },
+        { x: 1, y: 1.56 },
+        { x: 2, y: 1.42 },
+        { x: 3, y: 1.85 },
+        { x: 4, y: 1.62 },
+    ];
 
-// Add a line series.
-const lineSeries = chart.addLineSeries().setName("My data").add(data);
+    // Add a line series.
+    const lineSeries = chart.addLineSeries().setName("My data").add(data);
+});
