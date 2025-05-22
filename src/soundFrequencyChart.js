9778d92043ab4eea21604384183964d80eafe594
= <=>
2025-05-22 20:00:24 +0300

    one line left

--- src/soundFrequencyChart.js
+++ src/soundFrequencyChart.js
@@ -22,38 +22,6 @@ chart.axisX.setTickStrategy(AxisTickStrategies.DateTime)
 
 chart.setTitle('Sound Frequency')
 
-const diesel = [
-    { x: new Date(2022, 0, 1).getTime(), y: 1.52 },
-    { x: new Date(2022, 0, 2).getTime(), y: 1.52 },
-    { x: new Date(2022, 0, 3).getTime(), y: 1.58 },
-    { x: new Date(2022, 0, 4).getTime(), y: 1.52 },
-    { x: new Date(2022, 0, 5).getTime(), y: 2.0 },
-    { x: new Date(2022, 0, 6).getTime(), y: 2.0 },
-    { x: new Date(2022, 0, 7).getTime(), y: 2.0 },
-    { x: new Date(2022, 0, 8).getTime(), y: 2.0 },
-    { x: new Date(2022, 0, 9).getTime(), y: 2.26 },
-    { x: new Date(2022, 0, 10).getTime(), y: 1.9 },
-    { x: new Date(2022, 0, 11).getTime(), y: 1.9 },
-    { x: new Date(2022, 0, 12).getTime(), y: 1.9 },
-    { x: new Date(2022, 0, 13).getTime(), y: 1.9 },
-    { x: new Date(2022, 0, 14).getTime(), y: 1.6 },
-    { x: new Date(2022, 0, 15).getTime(), y: 1.6 },
-    { x: new Date(2022, 0, 16).getTime(), y: 1.6 },
-    { x: new Date(2022, 0, 17).getTime(), y: 1.0 },
-    { x: new Date(2022, 0, 18).getTime(), y: 1.0 },
-    { x: new Date(2022, 0, 19).getTime(), y: 1.0 },
-    { x: new Date(2022, 0, 20).getTime(), y: 1.74 },
-    { x: new Date(2022, 0, 21).getTime(), y: 1.47 },
-    { x: new Date(2022, 0, 22).getTime(), y: 1.47 },
-    { x: new Date(2022, 0, 23).getTime(), y: 1.47 },
-    { x: new Date(2022, 0, 24).getTime(), y: 1.74 },
-    { x: new Date(2022, 0, 25).getTime(), y: 1.74 },
-    { x: new Date(2022, 0, 26).getTime(), y: 1.74 },
-    { x: new Date(2022, 0, 27).getTime(), y: 1.5 },
-    { x: new Date(2022, 0, 28).getTime(), y: 1.5 },
-    { x: new Date(2022, 0, 29).getTime(), y: 1.5 },
-]
-
 const gasoline = [
     { x: new Date(2022, 0, 1).getTime(), y: 1.35 },
     { x: new Date(2022, 0, 2).getTime(), y: 1.35 },
@@ -87,12 +55,10 @@ const gasoline = [
 ]
 
 // Add two line series.
-const lineSeries = chart.addPointLineAreaSeries({ dataPattern: 'ProgressiveX' }).setAreaFillStyle(emptyFill).setName('Diesel')
 const lineSeries2 = chart.addPointLineAreaSeries({ dataPattern: 'ProgressiveX' }).setAreaFillStyle(emptyFill).setName('Gasoline')
 
 // Add the points to each Series
-lineSeries2.add(diesel)
-lineSeries.add(gasoline)
+lineSeries2.add(gasoline)
 
 // Setup view nicely.
 chart.axisY.setTitle('Price').setUnits('$/litre').setInterval({ start: 0, end: 3, stopAxisAfter: true })
