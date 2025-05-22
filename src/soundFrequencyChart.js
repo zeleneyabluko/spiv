d0527fdcc8614185cdfe68dee910e49b17ca2884
= <=>
2025-05-22 19:54:52 +0300

    removed one line

--- src/soundFrequencyChart.js
+++ src/soundFrequencyChart.js
@@ -22,38 +22,6 @@ chart.axisX.setTickStrategy(AxisTickStrategies.DateTime)
 
 chart.setTitle('Diesel and Gasoline Price Comparison')
 
-const diesel = [
-    { x: new Date(2022, 0, 1).getTime(), y: 1.52 },
-    { x: new Date(2022, 0, 2).getTime(), y: 1.52 },
-    { x: new Date(2022, 0, 3).getTime(), y: 1.58 },
-    { x: new Date(2022, 0, 4).getTime(), y: 1.52 },
-    { x: new Date(2022, 0, 5).getTime(), y: 2.0 },
-    { x: new Date(2022, 0, 6).getTime(), y: 2.0 },
-    { x: new Date(2022, 0, 7).getTime(), y: 2.0 },
-    { x: new Date(2022, 0, 8).getTime(), y: 2.0 },
-    { x: new Date(2022, 0, 9).getTime(), y: 2.26 },
-    { x: new Date(2022, 0, 10).getTime(), y: 1.9 },
-    { x: new Date(2022, 0, 11).getTime(), y: 1.9 },
-    { x: new Date(2022, 0, 12).getTime(), y: 1.9 },
-    { x: new Date(2022, 0, 13).getTime(), y: 1.9 },
-    { x: new Date(2022, 0, 14).getTime(), y: 1.6 },
-    { x: new Date(2022, 0, 15).getTime(), y: 1.6 },
-    { x: new Date(2022, 0, 16).getTime(), y: 1.6 },
-    { x: new Date(2022, 0, 17).getTime(), y: 1.0 },
-    { x: new Date(2022, 0, 18).getTime(), y: 1.0 },
-    { x: new Date(2022, 0, 19).getTime(), y: 1.0 },
-    { x: new Date(2022, 0, 20).getTime(), y: 1.74 },
-    { x: new Date(2022, 0, 21).getTime(), y: 1.47 },
-    { x: new Date(2022, 0, 22).getTime(), y: 1.47 },
-    { x: new Date(2022, 0, 23).getTime(), y: 1.47 },
-    { x: new Date(2022, 0, 24).getTime(), y: 1.74 },
-    { x: new Date(2022, 0, 25).getTime(), y: 1.74 },
-    { x: new Date(2022, 0, 26).getTime(), y: 1.74 },
-    { x: new Date(2022, 0, 27).getTime(), y: 1.5 },
-    { x: new Date(2022, 0, 28).getTime(), y: 1.5 },
-    { x: new Date(2022, 0, 29).getTime(), y: 1.5 },
-]
-
 const gasoline = [
     { x: new Date(2022, 0, 1).getTime(), y: 1.35 },
     { x: new Date(2022, 0, 2).getTime(), y: 1.35 },
